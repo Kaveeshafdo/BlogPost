d0e5a50a0449a34c635235e2e636d222119225dd
Kaveeshafdo <kaveesha.shavinda1999@gmail.com>
2022-02-21 22:57:14 +0530

    getdata

--- BlogPost/src/java/javaServerlet/Register.java
+++ BlogPost/src/java/javaServerlet/Register.java
@@ -1,6 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package javaServerlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,44 +14,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Register
+ *
+ * @author Kaveesha FDO
  */
-@WebServlet("/Register")
+@WebServlet(name = "Register", urlPatterns = {"/Register"})
 public class Register extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Register() {
-        super();
-        // TODO Auto-generated constructor stub
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String name = request.getParameter("name");
+            String email = request.getParameter("reg_email");
+            String pass = request.getParameter("reg_password");
+            String rePass = request.getParameter("repassword");
+            if (pass == rePass) {
+                if (name != null && email != null && pass != null) {
+                     response.sendRedirect("RegisterDao.");
+                } else {
+                    out.println("Empty Name, Email or Password");
+                }
+            }
+            else{
+             out.println("Invalid Password");
+            }
+
+        }
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String name = request.getParameter("name");
-		String email =  request.getParameter("email");
-		String pass =  request.getParameter("password");
-		String rePass =  request.getParameter("repassword");
-		
-		Member member = new Member(name,email,pass,rePass);
-		
-		RegisterDao rDao = new RegisterDao();
-		//String result = rDao.insert(member);
-		//response.getWriter().print(result);
-		
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
 
 }
