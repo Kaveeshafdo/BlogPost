e8644f0e8ff32d37de608e8cd1bada2a0db22168
Kaveeshafdo <kaveesha.shavinda1999@gmail.com>
2022-02-21 19:18:02 +0530

    register serverlet

---
+++ BlogPost/src/java/javaServerlet/Register.java
@@ -0,0 +1,51 @@
+package javaServerlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet("/Register")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String email =  request.getParameter("email");
+		String pass =  request.getParameter("password");
+		String rePass =  request.getParameter("repassword");
+		
+		Member member = new Member(name,email,pass,rePass);
+		
+		RegisterDao rDao = new RegisterDao();
+		String result = rDao.insert(member);
+		response.getWriter().print(result);
+		
+	}
+
+}
